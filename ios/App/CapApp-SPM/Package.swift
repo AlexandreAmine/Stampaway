489e74b26e3f4ac8f2164b10c5fd04913473e823
Jean Amine <jeanamine@Jeans-MacBook-Air-2020.local>
2026-05-12 14:39:32 +0100

    chore: configure iOS app and Capgo live updates

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,11 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
+<<<<<<< HEAD
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.2"),
+=======
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.3"),
+>>>>>>> 973d3b9 (chore: configure iOS app and Capgo live updates)
         .package(name: "CapacitorApp", path: "../../../node_modules/@capacitor/app"),
         .package(name: "CapacitorBrowser", path: "../../../node_modules/@capacitor/browser"),
         .package(name: "CapacitorCamera", path: "../../../node_modules/@capacitor/camera"),
